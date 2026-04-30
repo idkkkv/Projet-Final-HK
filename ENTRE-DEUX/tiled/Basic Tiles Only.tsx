94259ec585eb42ad8c3dea0bc577af873a027da5
Juliou <julien.bodou@outlook.fr>
2026-04-30 16:27:05 +0200

    map1

---
+++ ENTRE-DEUX/tiled/Basic Tiles Only.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Basic Tiles Only" tilewidth="16" tileheight="16" tilecount="4816" columns="86">
+ <image source="Basic Tiles Only.png" width="1376" height="896"/>
+</tileset>
---
+++ ENTRE-DEUX/tiled/ParallaxCave1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="ParallaxCave1" tilewidth="16" tileheight="16" tilecount="4896" columns="72">
+ <image source="Downloads/Cave Tileset Pack/PNG files/ParallaxCave1.png" width="1152" height="1088"/>
+</tileset>
